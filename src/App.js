77a1bb7342e43ed88e5cafccda5f5e705e6dfb43
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-08-30 13:09:59 -0400

    added placeholder navbar and cleaned app js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from './components/Navbar';
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
-    </div>
+    <Navbar />
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+export function Navbar () {
+    return (
+        <h1>
+        this is navbar
+        </h1>
+    )
+}
+
+export default Navbar
